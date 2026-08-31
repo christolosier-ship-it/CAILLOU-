acb1b47f285bfc3c4f36dfefe8abb23bfa2e8ca7
Kristo <christo.losier@gmail.com>
2026-08-31 21:22:59 +0200

    chore: make vite config import explicit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-import { pwaManifest } from './src/pwa/manifest'
+import { pwaManifest } from './src/pwa/manifest.ts'
 
 export default defineConfig({
   plugins: [
